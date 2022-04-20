f18fbd884d6cc919c12dbb24387770a22eccff52
simoneS96 <simone.stifano@incentergy.de>
2022-04-20 15:39:39 +0200

    continue setup

--- webapp/controller/medication.controller.js
+++ webapp/controller/medication.controller.js
@@ -1,11 +1,10 @@
 sap.ui.define([
 	'sap/ui/core/mvc/Controller',
-	'sap/m/MessageToast',
-	'sap/m/library'
-], function (Controller, MessageToast, library) {
+	'sap/m/MessageToast'
+], function (Controller, MessageToast) {
 	"use strict";
 
-	return Controller.extend("medication.controller.C", {
+	return Controller.extend("webapp.controller.medication", {
 
 		onAvatarPressed: function () {
 			MessageToast.show("Avatar pressed!");
