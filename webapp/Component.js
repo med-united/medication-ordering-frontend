16e6ddc249c3d36c0c284af896b760d9e1ebbcd4
Manuel Blechschmidt <manuel.blechschmidt@incentergy.de>
2022-05-07 22:53:37 +0200

    MEDU-26 Use keycloak to authenticate the user, MEDU-28 server now uses multitenancy

--- webapp/Component.js
+++ webapp/Component.js
@@ -13,6 +13,23 @@ sap.ui.define([
 		init: function() {
 			UIComponent.prototype.init.apply(this, arguments);
 			this.getRouter().initialize();
+			var oRouter = this.getRouter();
+			const keycloak = new Keycloak();
+            keycloak.init({"onLoad":"login-required"}).then(function(authenticated) {
+                console.log(authenticated ? 'authenticated' : 'not authenticated');
+				if(authenticated) {
+					// Add JWT token to all jQuery ajax requests
+					// This will work with the FHIR Model
+					// https://github.com/SAP/openui5-fhir/blob/v2.3.0/src/sap/fhir/model/r4/lib/FHIRRequestor.js#L324
+					$.ajaxSetup({
+						headers: { 'Authorization': 'Bearer ' + keycloak.token }
+					});
+					oRouter.navTo("overview");
+				}
+            }).catch(function(e) {
+                console.log('failed to initialize');
+            });
+
 		}
 	});
 
