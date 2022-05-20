5d1522e336259b5ede088d0beb723d8d70157c3b
ruszkipista <oi5672@gmail.com>
2022-05-20 13:44:16 +0100

    add Patient create example

--- webapp/medunited/care/utils/ProcessUpload.js
+++ webapp/medunited/care/utils/ProcessUpload.js
@@ -43,14 +43,14 @@ sap.ui.define([
                          { system: "email", value: oDataRow["PharmacyEMail"], use: "work" }]
                 }
                 oOrganisation.key = {
-                    name:    oOrganisation.data.name[0].family,
+                    name:    oOrganisation.data.name,
                     address: oOrganisation.data.address[0].line
                 };
                 oOrganisation.id = JSON.stringify(oOrganisation.key);
                 return oOrganisation;
             };            
 
-            const getPatient = function (oDataRow, sPractitionerId, sOrganisationId){
+            const getPatient = function (oDataRow){
                 const oPatient = {};
                 oPatient.data = {
                     name: 
@@ -58,32 +58,25 @@ sap.ui.define([
                            given: [oDataRow["PatientGivenName"]],
                            family: oDataRow["PatientFamilyName"] }],
                     birthdate: oDataRow["PatientBirthdate"],
-                    extension: [
-                        { practitioner: { reference: "" } },
-                        { organisation: { reference: "" } }
-                    ]
                 };
                 oPatient.key = {
                     family:    oDataRow["PatientFamilyName"],
                     given:     oDataRow["PatientGivenName"],
                     birthdate: oDataRow["PatientBirthdate"]
                 };
-                oPatient.setPractitionerReference = function (sPractitionerId){
-                    this.data.extension[0].practitioner.reference = "Practitioner/"+sPractitionerId;
-                };
-                oPatient.setOrganisationReference = function (sOrganisationId){
-                    this.data.extension[1].organisation.reference = "Organisation/"+sOrganisationId;
-                };
-                oPatient.setPractitionerReference(sPractitionerId);
-                oPatient.setOrganisationReference(sOrganisationId);
                 oPatient.id = JSON.stringify(oPatient.key);
+                oPatient.setPatientId = function (sPatientId){
+                    this.idInDB = sPatientId;
+                }
                 return oPatient;
             }
 
-            const getMedicationStatement = function (oDataRow, sPatientId){
+            const getMedicationStatement = function (oDataRow, sPatientId, sPractitionerId, sOrganisationId){
                 const oMedicationStatement = {};
                 oMedicationStatement.data = {
                     subject: { reference: "" },
+                    informationSource: { reference: "" },
+                    organisation: { reference: "" },
                     status: "Active",
                     medicationCodeableConcept: {
                         code: {
@@ -104,13 +97,22 @@ sap.ui.define([
                     ]
                 };
                 oMedicationStatement.key = {
-                    reference:  oMedicationStatement.data.subject.reference,
+                    subject:  oMedicationStatement.data.subject.reference,
+                    informationSource: oMedicationStatement.data.informationSource.reference,
                     medication: oMedicationStatement.data.medicationCodeableConcept.code.coding.code.value,
                     amount:     oMedicationStatement.data.medicationCodeableConcept.amount.numerator.value,
                 };
                 oMedicationStatement.setPatientReference = function (sPatientId){
                     this.data.subject.reference = "Patient/"+sPatientId;
                 };
+                oMedicationStatement.setPractitionerReference = function (sPractitionerId){
+                    this.data.informationSource.reference = "Practitioner/"+sPractitionerId;
+                };
+                oMedicationStatement.setOrganisationReference = function (sOrganisationId){
+                    this.data.organisation.reference = "Organisation/"+sOrganisationId;
+                };
+                oMedicationStatement.setPractitionerReference(sPractitionerId);
+                oMedicationStatement.setOrganisationReference(sOrganisationId);                
                 oMedicationStatement.setPatientReference(sPatientId);
                 oMedicationStatement.id = JSON.stringify(oMedicationStatement.key);
                 return oMedicationStatement;
@@ -121,14 +123,17 @@ sap.ui.define([
             oReader.onload = function (e) {
                 const oData = $.csv.toObjects(e.target.result);
                 const oBundle = {
+                    patients:[],
                     practitioners:[],
                     organisations: [],
-                    patients:[],
                     medicationStatements:[],
                 };
 
                 let isInBundle = false;
-                for (let oDataRow of oData) {
+                for (const oDataRow of oData) {
+                    const oPatient = getPatient(oDataRow);
+                    isInBundle = oBundle.patients.find(p=>p.id===oPatient.id);
+                    if (!isInBundle) oBundle.patients.push(oPatient);
 
                     const oPractitioner = getPractitioner(oDataRow);
                     isInBundle = oBundle.practitioners.find(p=>p.id===oPractitioner.id);
@@ -138,41 +143,32 @@ sap.ui.define([
                     isInBundle = oBundle.organisations.find(p=>p.id===oOrganisation.id);
                     if (!isInBundle) oBundle.organisations.push(oOrganisation);
 
-                    const oPatient = getPatient(oDataRow, oOrganisation.id, oPractitioner.id);
-                    isInBundle = oBundle.patients.find(p=>p.id===oPatient.id);
-                    if (!isInBundle) oBundle.patients.push(oPatient);
-
-                    const oMedicationStatement = getMedicationStatement(oDataRow, oPatient.id);
+                    const oMedicationStatement = getMedicationStatement(oDataRow, oPatient.id, oOrganisation.id, oPractitioner.id);
                     isInBundle = oBundle.medicationStatements.find(p=>p.id===oMedicationStatement.id);
                     if (!isInBundle) oBundle.medicationStatements.push(oMedicationStatement);
                 };
-                return;
+
                 // DB existence check
-                let sPatientId=null;
-                let oPatient = oBundle.patients[0];
-                var mParameters = {
-                    urlParameters: oPatient.key,
-                    success: function(oBundle){
-                        console.log(oBundle);
-                    },
-                    error: function(oError){
-                        console.log(oError.code, oError.message);
-                        sPatientId = oModel.create("Patient", oPatient.data, "patientDetails");
-                    }
-                };                    
-                oModel.sendGetRequest('/Patient', mParameters);
-
-
+                for (let oPatient of oBundle.patients) {
+                    var mParameters = {
+                        urlParameters: oPatient.key,
+                        success: function(oResponse){
+                            let sPatientId=null;
+                            if (!oResponse.entry){
+                                sPatientId = oModel.create("Patient", oPatient.data, "patientDetails");
+                            } else {
+                                sPatientId = oResponse.entry[0].id;
+                            }
+                            oPatient.setPatientId(sPatientId);
+                        },
+                        error: function(oError){
+                            console.log(oError.code, `${oError.message}\n${oError.additionalText}`);
+                        }
+                    };                    
+                    oModel.sendGetRequest('/Patient', mParameters);
+                    break;
+                }
                 oModel.submitChanges();
-
-                this.getView().getModel().create("MedicationStatement", {
-                    identifier: [{"value": sPZN}],
-                    dosage: [
-                        {text: sDosierschemaMorgens+"-"+sDosierschemaMittags+"-"+sDosierschemaAbends+"-"+sDosierschemaNachts}
-                    ],
-                    subject: {reference: "Patient/" + sPatientId}
-                }, "patientDetails");
-                                        
             };
             oReader.readAsBinaryString(oFile);
         }
