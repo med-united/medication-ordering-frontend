27a30d7eca5eae3e0293498707f8d170b66e3b8f
simoneS96 <102789473+simoneS96@users.noreply.github.com>
2022-05-30 15:44:52 +0200

    feat:MEDU-128 earztbrief from care frontend (#11)
    * make function to call earztbrief-sender
    
    * set xml model
    
    * change test email

--- webapp/medunited/care/controller/medication/Master.controller.js
+++ webapp/medunited/care/controller/medication/Master.controller.js
@@ -1,10 +1,16 @@
 sap.ui.define([
 	"medunited/base/controller/AbstractMasterController",
-	'medunited/care/utils/ScriptDownloader'
-], function (AbstractMasterController, ScriptDownloader) {
+	'medunited/care/utils/ScriptDownloader',
+	'medunited/care/utils/BriefSender',
+	'sap/ui/model/xml/XMLModel'
+], function (AbstractMasterController, ScriptDownloader, BriefSender, XMLModel) {
 	"use strict";
 
 	return AbstractMasterController.extend("medunited.care.controller.medication.Master", {
+
+		onInit: function () {
+			this.eArztbriefModel = new XMLModel("./medunited/template/Arztbrief-Minimal.XML");
+		},
 
 		getEntityName: function () {
 			return "Medication";
@@ -38,7 +44,8 @@ sap.ui.define([
 		},
 
 		onRequestEPrescriptions: function () {
-			ScriptDownloader.makePowershellScript(this.getView());
+			//ScriptDownloader.makePowershellScript(this.getView());
+			BriefSender.sendEarztBrief(this.getView(), this.eArztbriefModel);
 		}
 	});
 }, true);
---
+++ webapp/medunited/care/utils/BriefSender.js
@@ -0,0 +1,40 @@
+sap.ui.define([
+    'sap/ui/model/xml/XMLModel'
+], function (XMLModel) {
+    "use strict";
+    return {
+
+        sendEarztBrief: function (oView, earztbriefModel) {
+
+            console.log(earztbriefModel);
+
+            const oXmlDoc = earztbriefModel.getData();
+            const sXml = new XMLSerializer().serializeToString(oXmlDoc.documentElement);
+            console.log(sXml);
+
+            const patientGivenName = oView.getModel().getProperty("/Patient/1030/name/0/given/0");
+            const patientFamilyName = oView.getModel().getProperty("/Patient/1030/name/0/family");
+            const organizationEmail = oView.getModel().getProperty("/Patient/1030/managingOrganization/reference/telecom/1/value");
+
+            const templateParams = createRequestParams();
+
+            fetch('https://earztbrief-sender.med-united.health/sendEmail', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(templateParams)
+            });
+
+            function createRequestParams() {
+                return {
+                    contactname: patientGivenName + " " + patientFamilyName,
+                    contactemail: "simone.stifano@incentergy.de",
+                    contactmessage: earztbriefModel.getProperty("/component/structuredBody/component/section").toString(),
+                    attachment: sXml,
+                };
+            }
+        }
+    };
+}, true);
