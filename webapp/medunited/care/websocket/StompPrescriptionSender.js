7835d1685f661b4bdbacd728ed6ed742401fea98
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2022-07-12 12:38:38 +0100

    MEDU-210 add example bundle for prescription request creation

--- webapp/medunited/care/websocket/StompPrescriptionSender.js
+++ webapp/medunited/care/websocket/StompPrescriptionSender.js
@@ -1,8 +1,10 @@
 Object.assign(global, { WebSocket: require('websocket').w3cwebsocket });
 const StompJs = require('@stomp/stompjs');
+const exampleBundle = require('../../../resources/local/bundle.json');
 
 
 const client = new StompJs.Client({
+    // brokerURL: 'tcp://localhost:61616',  // to run locally
     brokerURL: 'wss://broker.med-united.health/stomp',
     connectHeaders: {
       login: 'admin',
@@ -22,7 +24,7 @@ client.onConnect = function (frame) {
 
     client.publish({
         destination: 'Prescriptions',
-        body: JSON.stringify({name: 'Beatriz'}),
+        body: JSON.stringify(exampleBundle),
         headers: { 'destination-type': 'ANYCAST', priority: '1', address: "Prescriptions", practiceManagementTranslation: 't2med', receiverPublicKeyFingerprint: 'SHA256:n3nqOvCrZkvW5Q4IFOd7NyhL2yL6l9sh7O2Iw4Clu1A' },
     });
 
