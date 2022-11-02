6c8f2084543805cb3d6edd88732be852fe88b005
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2022-11-02 11:26:43 +0000

    MED-385 initial commit

--- webapp/medunited/care/SharedBlocks/medication/MedicationBlockController.controller.js
+++ webapp/medunited/care/SharedBlocks/medication/MedicationBlockController.controller.js
@@ -7,8 +7,14 @@ sap.ui.define([
     "../../search/MedicationSearchProvider",
     "sap/ui/core/Item",
     "sap/m/MessageToast",
-    "sap/m/MessageBox"
-], function (AbstractController, Formatter, FHIRFilter, FHIRFilterType, FHIRFilterOperator, MedicationSearchProvider, Item, MessageToast, MessageBox) {
+    "sap/m/MessageBox",
+    "sap/m/p13n/Engine",
+    'sap/m/p13n/SelectionController',
+	'sap/m/p13n/SortController',
+	'sap/m/p13n/GroupController',
+	'sap/m/p13n/MetadataHelper',
+	'sap/ui/model/Sorter'
+], function (AbstractController, Formatter, FHIRFilter, FHIRFilterType, FHIRFilterOperator, MedicationSearchProvider, Item, MessageToast, MessageBox, Engine, SelectionController, SortController, GroupController, MetadataHelper, Sorter) {
     "use strict";
 
     return AbstractController.extend("medunited.care.SharedBlocks.medication.MedicationBlockController", {
@@ -36,6 +42,8 @@ sap.ui.define([
             // lcp --proxyUrl https://www.apotheken-umschau.de/
             // http://localhost:8010/proxy
             this._oMedicationSearchProvider.setSuggestUrl("https://medication.med-united.health/ajax/search/drugs/auto/?query={searchTerms}");
+
+            this._registerForP13n();
         },
 
         initializeRouter: function () {
@@ -174,6 +182,88 @@ sap.ui.define([
             var source = oEvent.getSource();
             source.getModel().setProperty(source.getBindingContext().getPath("medicationCodeableConcept/text"), medicationName);
             source.getModel().setProperty(source.getBindingContext().getPath("identifier/0/value"), medicationPZN);
-        }
+        },
+        openPersoDialog: function(oEvent) {
+			let oTable = this.byId("medicationTable");
+
+			Engine.show(oTable, ["Columns", "Sorter", "Groups"], {
+				contentHeight: "35rem",
+				contentWidth: "32rem",
+				source: oEvent.getSource()
+			});
+		},
+        _registerForP13n: function() {
+			let oTable = this.byId("medicationTable");
+
+			this.oMetadataHelper = new MetadataHelper([
+				{key: "__xmlview2--medicationBlock-defaultXML--medicationName", label: "Name", path: "medicationCodeableConcept/text"},
+                {key: "__xmlview2--medicationBlock-defaultXML--medicationPZN", label: "PZN", path: "identifier/0/value"},
+                {key: "__xmlview2--medicationBlock-defaultXML--medicationDosage", label: "Dosage", path: "dosage/0/text"},
+                {key: "__xmlview2--medicationBlock-defaultXML--medicationSize", label: "Size", path: "extension/0/valueString"},
+                {key: "__xmlview2--medicationBlock-defaultXML--medicationStandardSize", label: "Standard Size", path: "extension/1/valueString"},
+                {key: "__xmlview2--medicationBlock-defaultXML--medicationNote", label: "Note", path: "note/0/text"}
+			]);
+            console.log("metadatahelper:", this.oMetadataHelper)
+
+			Engine.register(oTable, {
+				helper: this.oMetadataHelper,
+				controller: {
+					Columns: new SelectionController({
+						targetAggregation: "columns",
+						control: oTable
+					}),
+					Sorter: new SortController({
+						control: oTable
+					}),
+					Groups: new GroupController({
+						control: oTable
+					})
+				}
+			});
+            console.log("before attachStateChange")
+			Engine.attachStateChange(this.handleStateChange.bind(this));
+            console.log(Engine)
+            console.log("after attachStateChange")
+		},
+        handleStateChange: function(oEvent) {
+            console.log("entered")
+            console.log(oEvent)
+			let oTable = this.byId("medicationTable");
+			let oState = oEvent.getParameter("state");
+
+			oTable.getColumns().forEach(function(oColumn, iIndex){
+                console.log("column: ", oColumn)
+				oColumn.setVisible(false);
+			});
+
+			oState.Columns.forEach(function(oProp, iIndex){
+				let oCol = this.byId(oProp.key);
+				oCol.setVisible(true);
+
+				let iOldIndex = oTable.getColumns().indexOf(oCol);
+
+				oTable.removeColumn(oCol);
+				oTable.insertColumn(oCol, iIndex);
+
+				oTable.getItems().forEach(function(oItem){
+					if (oItem.isA("sap.m.ColumnListItem")) {
+						let oCell = oItem.getCells()[iOldIndex];
+						oItem.removeCell(oCell);
+						oItem.insertCell(oCell, iIndex);
+					}
+				});
+			}.bind(this));
+
+			let aSorter = [];
+			oState.Sorter.forEach(function(oSorter) {
+				aSorter.push(new Sorter(this.oMetadataHelper.getPath(oSorter.key), oSorter.descending));
+			}.bind(this));
+
+			oState.Groups.forEach(function(oGroup) {
+				aSorter.push(new Sorter(this.oMetadataHelper.getPath(oGroup.key), oGroup.descending, true));
+			}.bind(this));
+
+			oTable.getBinding("items").sort(aSorter);
+		}
     });
 });
