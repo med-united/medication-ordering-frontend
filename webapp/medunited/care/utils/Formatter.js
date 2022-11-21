6760449ca3f6d1558b118e565d32de74663588a3
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2022-11-21 08:13:42 +0000

    display patient age next to birthdate in patient details

--- webapp/medunited/care/utils/Formatter.js
+++ webapp/medunited/care/utils/Formatter.js
@@ -9,20 +9,44 @@ sap.ui.define(["sap/ui/core/format/DateFormat"], function(DateFormat) {
             return sUse ? (sUse.charAt(0).toUpperCase() + sUse.slice(1)+ ":") : "";
         },
 
-		formatBirthDate: function(sDate) {
+		formatBirthDateAndAge: function(sDate) {
 			if (sDate) {
-				var dateFormat = DateFormat.getDateTimeInstance({
+				const dateFormat = DateFormat.getDateTimeInstance({
 					pattern: "dd.MM.yyyy"
 				});
-				return dateFormat.format(new Date(sDate));
-			}
 
+                let birthDate =  dateFormat.format(new Date(sDate));
+                const birthDateParts = birthDate.split(".");
+                let dd = birthDateParts[0];
+                let mm = birthDateParts[1];
+                let yyyy = birthDateParts[2];
+
+                let currentDate = new Date();
+                let currentYear = currentDate.getFullYear();
+                let currentMonth = currentDate.getMonth();
+                let currentDay = currentDate.getDate(); 
+                let calculatedAge = currentYear - yyyy;
+
+                if (currentMonth < mm - 1) {
+                    calculatedAge--;
+                }
+                if (mm - 1 == currentMonth && currentDay < dd) {
+                    calculatedAge--;
+                }
+                if (calculatedAge < 0) {
+                    return "Falsches geburtsdatum eingestellt! " + birthDate;
+                }
+                if (calculatedAge == 1) {
+                    return birthDate + " - " + calculatedAge + " Jahr";
+                }
+				return birthDate + " - " + calculatedAge + " Jahre";
+			}
 			return "";
         },
 
         formatFullTime: function(sDate){
             if (sDate) {
-				var dateFormat = DateFormat.getDateTimeInstance({
+				const dateFormat = DateFormat.getDateTimeInstance({
 					pattern: "dd.MM.yyyy hh:mm:ss"
 				});
 				return dateFormat.format(new Date(sDate));
